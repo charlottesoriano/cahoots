0f28b1b96c690d76374784b986509b46cba47c3a
charlotte <charlottesoriano205@gmail.com>
2026-09-16 22:20:15 +0800

    phase 1 auth completion

--- backend/routers/events.py
+++ backend/routers/events.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
+from core.security import get_current_user
+from fastapi import Depends
 
 router = APIRouter(
     prefix="/events",
     tags=["events"],
+    dependencies=[Depends(get_current_user)] # only authenticated users can access this router
 )
--- backend/routers/polls.py
+++ backend/routers/polls.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
+from core.security import get_current_user
+from fastapi import Depends
 
 router = APIRouter(
     prefix="/polls",
     tags=["polls"],
-)
+    dependencies=[Depends(get_current_user)] # only authenticated users can access this router
+)   
